afba3625fc95caf91387a3ca52fb965b74ae6b1c
중화이 <ljh15952@gmail.com>
2023-01-07 12:56:30 +0000

    v0.0.3

--- Source/Tetris.js
+++ Source/Tetris.js
@@ -4,7 +4,43 @@ let cfx = canvas.getContext('2d')
 let wall = {new:Array(20).fill(0).map(()=>Array(10).fill(0)), 
 			old:Array(20).fill(0).map(()=>Array(10).fill(0))};
 
-wall.old[5][5] = 1; //검은색 블록이 잘 찍히나 테스트 용
+let pos = {x:5,y:-2};
+const tets = [
+  [['□', '■', '□'], ['■', '■', '■'], ['□', '□', '□']], 
+  [['■', '□', '□'], ['■', '■', '■'], ['□', '□', '□']], 
+  [['□', '□', '■'], ['■', '■', '■'], ['□', '□', '□']], 
+  [['■', '■', '□'], ['■', '■', '□'], ['□', '□', '□']], 
+  [['□', '■', '■'], ['■', '■', '□'], ['□', '□', '□']], 
+  [['■', '■', '□'], ['□', '■', '■'], ['□', '□', '□']],
+  [['□', '□', '□', '□'], ['■', '■', '■', '■'], ['□', '□', '□', '□']], // I
+];
+let tet = tets[Math.floor(Math.random() * tets.length)];
+
+
+
+const setCoords = (t, p) =>
+t.map((r, i) => 
+	  r.map((c, j) => 
+			({ x: p.x + j, y: p.y + i, z: c == '■' }))).reduce((acc,val)=>acc.concat(val), []);
+
+let coords = setCoords(tet,pos);
+
+const placeOnWell = (c,w) => {
+	c.forEach(b => {
+		if(b.y >= 0 && b.z){
+			w[b.y][b.x] = 1;
+		}
+	});
+};
+
+const removeFromWell = (c,w) => {
+	const ww = w;
+	coords.forEach(b => {
+		if(b.y >= 0 && b.z){
+			ww[b.y][b.x] = 0;
+		}
+	});
+};
 
 const renderWall = () => {
 	wall.old.map((a,i)=>{
@@ -27,7 +63,24 @@ const renderWall = () => {
 	});
 }
 
+const canMove = (dir) => {
+	return true;	
+}
+
+const move = (dir) => {
+	removeFromWell(coords,wall.new);
+	if(dir == 'down') { pos.y += 1; }
+	coords = setCoords(tet,pos);
+	placeOnWell(coords,wall.new);
+}
+
+let before = Date.now();
 const update = () => {
+	let current = Date.now();
+	if(current - before >= 350){
+		before = current;
+		canMove('down') && move('down');
+	}
 	renderWall();
 	requestAnimationFrame(update);
 }
